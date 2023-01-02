b19a983644d854aa3fa2f4872e4bb494f831ca79
Bruce Wang <number5@users.noreply.github.com>
2023-01-02 22:44:06 +1100

    updates

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -42,13 +42,13 @@
 
   # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "bruce";
+    homeDirectory = "/home/bruce";
   };
 
   # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  programs.neovim.enable = true;
+  home.packages = with pkgs; [ steam ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
