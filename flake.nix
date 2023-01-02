05012b43af72d4d003e61be8a30d90578da17a90
Bruce Wang <number5@users.noreply.github.com>
2023-01-03 03:33:52 +1100

    wip

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
         inherit pkgs system username hostName stateVersion;
         lib = nixpkgs.lib;
       };
-      homeConfigurations."${username}}@${hostName}" = mkHome {
+      homeConfigurations."${username}}@${hostName}" = homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.${system};
         modules = [
           ./home/home.nix
