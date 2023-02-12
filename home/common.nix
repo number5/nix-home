15f30df1cb99df74c96e118877b5f921ab8d71c1
Bruce Wang <number5@users.noreply.github.com>
2023-02-12 23:54:01 +1100

    fix home

---
+++ home/common.nix
@@ -0,0 +1,56 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  imports = [
+    #./modules/neovim.nix
+    ./programs
+    ./services
+  ];
+
+  config = {
+    home.packages = with pkgs; [
+      # Applications
+      vlc
+
+      # Desktop
+      htop-vim
+      nvtop
+      feh
+      playerctl
+      xbanish
+      neofetch
+      xclip
+      ripgrep
+      xmousepasteblock
+      jq
+      unzip
+      p7zip
+      unrar
+
+      # Programming
+      asdf-vm
+      nix-prefetch-git
+      yarn
+      rnix-lsp
+      postman
+      nasm
+      esbuild
+    ];
+    #  unstablePackages = with pkgs.unstable; [
+    #    # Applications
+    #    ledger-live-desktop
+    #    #Libraries
+    #    ffmpeg-full
+    #  ];
+
+    home.enableNixpkgsReleaseCheck = false;
+
+    home.stateVersion = "22.11";
+    home.username = "bruce";
+    home.homeDirectory = "/home/bruce";
+    programs.home-manager.enable = true;
+  };
+}
