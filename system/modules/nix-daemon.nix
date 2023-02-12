1bf89ceea4b7939f57e20cc513214e2f069234e6
Bruce Wang <number5@users.noreply.github.com>
2023-02-13 03:27:54 +1100

    fix builder

--- system/modules/nix-daemon.nix
+++ system/modules/nix-daemon.nix
@@ -20,13 +20,9 @@
       fsync-metadata = lib.boolToString (!config.boot.isContainer or config.fileSystems."/".fsType != "zfs");
       substituters = [
         "https://nix-community.cachix.org"
-        #"https://mic92.cachix.org"
-        "https://cache.thalheim.io"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        #"mic92.cachix.org-1:gi8IhgiT3CYZnJsaW7fxznzTkMUOn1RY4GmXdT/nXYQ="
-        "cache.thalheim.io-1:R7msbosLEZKrxk/lKxf9BTjOOH7Ax3H0Qj0/6wiHOgc="
       ];
 
       trusted-users = ["@wheel" "root"];
@@ -37,9 +33,9 @@
     };
   };
 
-  imports = [
-    ./builder.nix
-  ];
+  #imports = [
+  #  ./builder.nix
+  #];
 
   programs.command-not-found.enable = false;
 }
