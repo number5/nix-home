bfe6ab31744eb43a05864a7902a4ec96eb71c3f5
Bruce Wang <number5@users.noreply.github.com>
2023-02-14 04:18:08 +1100

    fix steam

--- system/modules/misc.nix
+++ system/modules/misc.nix
@@ -4,6 +4,9 @@
   lib,
   ...
 }: {
+  # Steam fix
+  hardware.opengl.driSupport32Bit = true;
+
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
