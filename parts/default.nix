aac3e691eb8c0d888368190c270bff746d8945ac
Bruce Wang <number5@users.noreply.github.com>
2023-03-27 05:05:57 +1100

    refactor p2

---
+++ parts/default.nix
@@ -0,0 +1,14 @@
+_inputs: {
+  imports = [
+    # Order-sensitive
+    #./overlays.nix
+    # Order-independent
+    #./checks.nix
+    #./darwin.nix
+    #./deploy.nix
+    #./devshell.nix
+    ./home-manager.nix
+    ./nixos.nix
+    #./packages.nix
+  ];
+}
