43abe18ee6d4c86aa15bc7e9b00436b244c714db
Bruce Wang <number5@users.noreply.github.com>
2023-08-28 00:22:31 +1000

    fix hardware

--- system/machines/chestnut/configuration.nix
+++ system/machines/chestnut/configuration.nix
@@ -38,7 +38,7 @@
       enable = true;
       layout = "us";
       xkbOptions = "caps:escape"; # Caps-lock is the new Escape.
-      # videoDrivers = ["amdgpu"];
+      videoDrivers = ["amdgpu"];
       libinput = {
         enable = true;
         mouse = {
