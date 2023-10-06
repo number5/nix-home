61bdd1a5a1e9c769d9981b7928a69abea8befc46
Bruce Wang <number5@users.noreply.github.com>
2023-10-07 04:20:00 +1100

    pkgs

---
+++ packages/default.nix
@@ -0,0 +1,21 @@
+{inputs, ...}: {
+  _file = ./default.nix;
+
+  perSystem = {
+    system,
+    pkgs,
+    lib,
+    inputs',
+    ...
+  }: let
+    upkgs = inputs'.nixpkgs-unstable.legacyPackages;
+
+  in {
+    packages = lib.mkMerge [
+      {
+
+        alejandra = inputs'.alejandra.packages.default;
+      }
+    ];
+  };
+}
