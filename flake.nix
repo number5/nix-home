44eebaa2bb2025f777bd82129ec0b2d7a6cfff0a
Bruce Wang <number5@users.noreply.github.com>
2024-05-27 02:17:40 +1000

    updates

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   inputs = {
-    # Principle inputs (updated by `nix run .#update`)
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager";
@@ -21,7 +20,10 @@
     nix-minecraft.url = "github:Infinidoge/nix-minecraft";
     nix-minecraft.inputs.nixpkgs.follows = "nixpkgs";
 
-    # impermanence.url = "github:nix-community/impermanence";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     dotzsh.url = "github:number5/dotzsh";
     dotzsh.flake = false;
@@ -62,7 +64,7 @@
                     ./home/common.nix
                     ./home/home-manager-options.nix
                   ];
-                  home.stateVersion = "23.05";
+                  home.stateVersion = "23.11";
                 };
               }
             ];
