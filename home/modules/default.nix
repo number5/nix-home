700e76b603246ae5eede62c67cf60287c98b1e0b
Bruce Wang <number5@users.noreply.github.com>
2024-07-07 04:31:54 +1000

    updates

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -6,12 +6,10 @@ inputs: {
 
   #  "i3" = import ./i3 inputs;
   # "polybar" = import ./polybar inputs;
+  "hyprland" = import ./wayland/hyprland inputs;
   "libvirt" = import ./libvirt.nix inputs;
   "apps" = import ./apps.nix inputs;
   "dunst" = import ./dunst inputs;
   "anyrun" = import ./anyrun inputs;
   "eww" = import ./wayland/eww inputs;
-  "swappy" = import ./swappy.nix inputs;
-  "hyprland" = import ./wayland/hyprland inputs;
-  # "base" = import ./profiles/base.nix inputs; # base profile
 }
