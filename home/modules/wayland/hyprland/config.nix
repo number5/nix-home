2aead2317000165227f7f158e9e4c00a0e6dde8b
Bruce Wang <number5@users.noreply.github.com>
2024-07-22 08:58:57 +1000

    take 2

--- home/modules/wayland/hyprland/config.nix
+++ home/modules/wayland/hyprland/config.nix
@@ -16,6 +16,21 @@
   in "pkill ${prog} || ${runserv} ${program}";
 in {
   wayland.windowManager.hyprland = {
+    enable = true;
+
+    # plugins = with inputs.hyprland-plugins.packages.${pkgs.system}; [
+    #   # hyprbars
+    #   # hyprexpo
+    # ];
+
+    systemd = {
+      variables = ["--all"];
+      extraCommands = [
+        "systemctl --user stop graphical-session.target"
+        "systemctl --user start hyprland-session.target"
+      ];
+    };
+
     settings = {
       "$MOD" = "SUPER";
       env = [
--- home/modules/wayland/hyprland/default.nix
+++ home/modules/wayland/hyprland/default.nix
@@ -18,33 +18,15 @@
   };
   rules = import ./rules.nix;
 in {
-  imports = [ rules ];
+  imports = [rules];
   config =
-    hypr-config
-    // {
+    {
       home = {
         packages = with pkgs; [
           seatd
           jaq
         ];
       };
-
-      # enable hyprland
-      wayland.windowManager.hyprland = {
-        enable = true;
-
-        # plugins = with inputs.hyprland-plugins.packages.${pkgs.system}; [
-        #   # hyprbars
-        #   # hyprexpo
-        # ];
-
-        systemd = {
-          variables = ["--all"];
-          extraCommands = [
-            "systemctl --user stop graphical-session.target"
-            "systemctl --user start hyprland-session.target"
-          ];
-        };
-      };
-    };
+    }
+    // hypr-config;
 }
