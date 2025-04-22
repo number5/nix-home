9c09922f9e00f7ad8e24da41e512969fcc1e9702
Bruce Wang <number5@users.noreply.github.com>
2025-04-23 05:16:23 +1000

    reduce one level

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -6,11 +6,11 @@ inputs: {
 
   #  "i3" = import ./i3 inputs;
   # "polybar" = import ./polybar inputs;
-  "hyprland" = import ./wayland/hyprland inputs;
+  "hyprland" = import ./hyprland inputs;
   "libvirt" = import ./libvirt.nix inputs;
   "apps" = import ./apps.nix inputs;
   "dunst" = import ./dunst inputs;
-  "eww" = import ./wayland/eww inputs;
+  "eww" = import ./eww inputs;
   "foot" = import ./foot inputs;
 
   "dotfiles" = import ./dotfiles inputs;
